8289d4bd3d59e65860f290312b16550dc227e8be
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-12-16 19:14:57 -0300

    feat: sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,67 @@
-import Image from "next/image"
+// Importação de ícones
+import { Home as HomeIcon, Search as SearchIcon, Library as LibraryIcon } from "lucide-react"
 
+/** Componente da página inicial do Spotify. */
 export default function Home() {
   return (
-    /*
-     * - `h-screen`: ocupar toda a altura da tela (`height: 100%`)
-     * - `flex flex-col`: layout flex em coluna
-     */
     <div className="h-screen flex flex-col">
       {/* Interface */}
-      {/*
-       * - `flex`: flexbox
-       */}
       <div className="flex flex-1">
         {/* Sidebar */}
-        {/*
-         * - `w-`: largura (width)
-         */}
-        <aside className="w-64 bg-zinc-950 p-6">sidebar</aside>
+        <Sidebar />
 
         {/* Conteúdo principal (músicas) */}
-        {/*
-         * - `flex-1`: ocupar o espaço restante dentro da flexbox
-         */}
         <main className="flex-1 p-6">main</main>
       </div>
 
       {/* Barra do player */}
       <footer className="bg-zinc-800 border-t border-zinc-700 px-6 p-6">footer</footer>
     </div>
+  )
+}
+
+/** Componente da sidebar. */
+function Sidebar() {
+  /** Lista de links do navbar. */
+  const navList: NavLinkProps[] = [
+    { Icon: HomeIcon, text: "Home" },
+    { Icon: SearchIcon, text: "Search" },
+    { Icon: LibraryIcon, text: "Your Library" },
+  ]
+
+  return (
+    <aside className="w-72 bg-zinc-950 p-6">
+      {/* Barra de navegação */}
+      <nav className="space-y-5">
+        {/* Iteração para gerar os links */}
+        {navList.map((link) => (
+          <NavLink Icon={link.Icon} text={link.text} link={link.link} />
+        ))}
+      </nav>
+    </aside>
+  )
+}
+
+/** Interface dos props do componente `NavLink`. */
+interface NavLinkProps {
+  /** Ícone do link. */
+  Icon: React.ComponentType
+  /** Texto do link. */
+  text: string
+  /** Endereço do link. */
+  link?: string
+}
+
+/** Componente do link do navbar. */
+function NavLink({ Icon, text, link = "" }: NavLinkProps) {
+  return (
+    <a
+      href={link} // Endereço
+      // Classes CSS:
+      className="flex items-center gap-3 text-sm font-semibold text-zinc-200"
+    >
+      <Icon /> {/* Ícone */}
+      {text} {/* Texto */}
+    </a>
   )
 }
