367899bf7e0077a87b15df3f10c20697240ccb76
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-12-17 17:26:32 -0300

    feat: botões principais do player

--- src/app/layout/PlayerControl.tsx
+++ src/app/layout/PlayerControl.tsx
@@ -2,6 +2,8 @@
 import Image, { StaticImageData } from "next/image"
 // importação das imagens
 import placeholder from "../../../public/playlistCovers/placeholder.jpeg"
+// importação de ícones
+import { PlayIcon, RepeatIcon, ShuffleIcon, SkipBackIcon, SkipForwardIcon } from "lucide-react"
 
 /** Componente dos controles de players exibido no canto inferior da tela. */
 export default function PlayerControl() {
@@ -50,7 +52,39 @@ function MusicInfo({ cover = placeholder, music, artist }: MusicInfoProps) {
 
 /** Componente do controle principal. */
 function Control() {
-  return <div></div>
+  return (
+    <div className="flex flex-col items-center">
+      {/* Botões de controle */}
+      <div className="flex items-center gap-6">
+        {/* Botão aleatorizar */}
+        <IconButton Icon={ShuffleIcon} />
+        {/* Botão anterior */}
+        <IconButton Icon={SkipBackIcon} />
+        {/* Botão de play */}
+        <button className="pd-2 rounded-full bg-white text-lg w-10 h-10 flex items-center justify-center">
+          <PlayIcon fill="bg-black" strokeWidth={0} />
+        </button>
+        {/* Botão próxima */}
+        <IconButton Icon={SkipForwardIcon} />
+        {/* Botão loop */}
+        <IconButton Icon={RepeatIcon} />
+      </div>
+    </div>
+  )
+}
+/** Interface dos props do componente `IconButton`. */
+interface IconButtonProps {
+  /** Ícone. */
+  Icon: React.ComponentType<{
+    /** Tamanho do ícone. */
+    size: number
+    /** Classes CSS personalizadas. (opcional) */
+    className?: string
+  }>
+}
+/** Componente do botão de ícone. */
+function IconButton({ Icon }: IconButtonProps) {
+  return <Icon size={20} className="text-zinc-200 cursor-pointer" />
 }
 
 /** Componente dos botões auxiliares */
